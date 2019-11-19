b0399dbaf64f70f223aeaad2dfc38c125f5eebbf
Griffin Yacynuk <griffin@ritual.co>
2019-11-19 00:40:53 -0500

    Updates

---
+++ precache-manifest.547572aefbad87a7bca3b0b5cafb5af0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c72ea70c37042bf86ba26b9ff7e5104a",
+    "url": "/DiffTool/index.html"
+  },
+  {
+    "revision": "d14d5269568ed8aa5ff7",
+    "url": "/DiffTool/static/css/main.b257d8d2.chunk.css"
+  },
+  {
+    "revision": "d142012791ea3078bb6b",
+    "url": "/DiffTool/static/js/2.ee72128e.chunk.js"
+  },
+  {
+    "revision": "d14d5269568ed8aa5ff7",
+    "url": "/DiffTool/static/js/main.89b71446.chunk.js"
+  },
+  {
+    "revision": "4a599a49710f0698c616",
+    "url": "/DiffTool/static/js/runtime-main.b3579e94.js"
+  }
+]);
